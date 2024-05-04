f2146e460ed6febe37e6ffcc58258b7684428d19
kokosko66 <rg.raikov.eu@mgmail.com>
2024-05-04 23:57:21 +0300

    Landing page

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import IndexPage from './pages/IndexPage'
+import LoginPage from './pages/LoginPage'
+
+function App() {
+
+  return (
+
+    <Routes>
+      <Route index element={<IndexPage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+    </Routes>
+        
+  )
+}
+
+export default App
